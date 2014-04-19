b0aa6689831607499739a504a8b9a4b2161e317f
Kenichiro Hiraiwa <Kenichiro@Kenichiro-PC.(none)>
2014-04-19 15:23:32 +0900

    initial commit.

---
+++ src/main/java/com/msk/flatrock/SubApplication.java
@@ -0,0 +1,19 @@
+package com.msk.flatrock;
+
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class SubApplication extends Application {
+    public void launchWrapper(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/main/scala/com/msk/flatrock/Main.scala
@@ -0,0 +1,40 @@
+package com.msk.flatrock
+
+import javafx.application.Application
+import javafx.stage.Stage
+import javafx.scene.Scene
+import javafx.scene.Group
+
+class Main extends Application {
+  override def start(stage: Stage): Unit = {
+    stage.setScene(new Scene(new Group()))
+    stage.show()
+  }
+  
+  def launch(args: Array[String]): Unit = {
+    Application.launch("", "", "")
+  }
+
+  def testOption: Option[Int] = {
+    var i: Int = 0
+    if ( i == 0) Some( 1 )
+    else None
+  }
+}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val app:Main = new Main
+    app.launch(args)
+    
+    val file = app.testOption
+    var str:String = null
+    file match {
+        case Some( f ) => str = "exist! [%d]" format f
+        case None  => str = "nothing!"
+    }
+    
+    println(str)
+  }
+  
+}
